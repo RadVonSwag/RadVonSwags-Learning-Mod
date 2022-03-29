6e10bc8772dd8eb1c546b8482bbc3555c2fac895
RadVonSwag <drewstes1@gmail.com>
2022-03-28 20:17:09 -0500

    added lean

--- src/main/java/net/radslearning/Main/RadsLearning.java
+++ src/main/java/net/radslearning/Main/RadsLearning.java
@@ -1,6 +1,12 @@
 package net.radslearning.Main;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +16,16 @@ public class RadsLearning implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("radslearning");
 
+	public static final Item LEAN = new Item(new FabricItemSettings().group(ItemGroup.BREWING));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("lean", "lean"), LEAN);
+		
+		//LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/radslearning/Main/mixin/ExampleMixin.java
+++ src/main/java/net/radslearning/Main/mixin/ExampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		RadsLearning.LOGGER.info("This line is printed by an example mod mixin!");
+		RadsLearning.LOGGER.info("I got no idea what mixins do lol");
 	}
 }
