2ef8fd83626c4cd2f48ce9622ece2fd3bfca7100
RadVonSwag <drewstes1@gmail.com>
2022-04-04 23:09:04 -0500

    Trying to make the lean apply the leaning effect

--- src/main/java/net/radslearning/Main/item/Lean.java
+++ src/main/java/net/radslearning/Main/item/Lean.java
@@ -1,11 +1,36 @@
 package net.radslearning.Main.item;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import net.radslearning.Main.statuseffects.Leaning;
 
 public class Lean extends Item {
 
     public Lean(Settings settings) {
         super(settings);
     }
+
+    @Override
+    public SoundEvent getDrinkSound() {
+        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatSound() {
+        return SoundEvents.ITEM_HONEY_BOTTLE_DRINK;
+    }
     
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        playerEntity.addStatusEffect(new Leaning(), null); //brokey how do fix?
+        return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    }
 }
