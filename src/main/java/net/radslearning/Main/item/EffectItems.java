604906c975536a7bba1089e98a2f27bd14105a34
RadVonSwag <drewstes1@gmail.com>
2022-04-04 23:10:06 -0500

    trying to make the lean make you lean

---
+++ src/main/java/net/radslearning/Main/item/EffectItems.java
@@ -0,0 +1,12 @@
+package net.radslearning.Main.item;
+
+import net.minecraft.item.Item;
+
+public class EffectItems extends Item {
+
+    public EffectItems(Settings settings) {
+        super(settings);
+        //TODO Create an effect Items class that lets items have effects lol
+    }
+    
+}
