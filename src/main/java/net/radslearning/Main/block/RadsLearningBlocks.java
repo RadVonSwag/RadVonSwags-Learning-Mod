4085c1342984b21564b696cde286db535e968802
RadVonSwag <drewstes1@gmail.com>
2022-06-22 15:57:41 -0500

    Added Lemon Wood Stairs

--- src/main/java/net/radslearning/Main/block/RadsLearningBlocks.java
+++ src/main/java/net/radslearning/Main/block/RadsLearningBlocks.java
@@ -7,11 +7,14 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.Material;
 import net.minecraft.block.PillarBlock;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -33,6 +36,8 @@ public class RadsLearningBlocks {
     public static final Block STRIPPED_LEMON_WOOD = add("stripped_lemon_wood", new PillarBlock(FabricBlockSettings.copyOf(Blocks.STRIPPED_OAK_WOOD)));
     public static final Block LEMON_WOOD_LEAVES = add("lemon_wood_leaves", new LeavesBlock(FabricBlockSettings.copyOf(Blocks.OAK_LEAVES).nonOpaque()));
     public static final Block LEMON_WOOD_SAPLING = add("lemon_wood_sapling", new ModSaplingBlock(new LemonWoodSaplingGenerator(), FabricBlockSettings.copyOf(Blocks.OAK_SAPLING)));
+    public static final Block LEMON_WOOD_STAIRS = add("lemon_wood_stairs",new RadsStairsBlock(LEMON_WOOD_PLANKS.getDefaultState(), AbstractBlock.Settings.copy(LEMON_WOOD_PLANKS)));
+
 
     private static <B extends Block> B add(String name, B block) {
         BLOCKS.put(new Identifier(RadsLearning.MOD_ID, name), block);
@@ -65,5 +70,13 @@ public class RadsLearningBlocks {
 
     private static void registerStrippableBlocks() {
         StrippableBlockRegistry.register(LEMON_WOOD_LOG, STRIPPED_LEMON_WOOD_LOG);
+    }
+
+    private static class RadsStairsBlock extends StairsBlock {
+
+        protected RadsStairsBlock(BlockState baseBlockState, Settings settings) {
+            super(baseBlockState, settings);
+        }
+    
     }
 }
